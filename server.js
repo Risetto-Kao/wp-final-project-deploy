d0e2bc1fba56358e37d0403df715c631dd159bad
Risetto-Kao <max52107429@gmail.com>
2021-06-29 23:55:53 +0800

    23:55 test

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ import "dotenv-defaults/config.js";
 import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import mongo from "./backend/src/mongo.js";
-import apiRoute from "./backend/routes/api/index.js";
+import dotenv from 'dotenv-defaults';
 
+import mongo from "./backend/src/mongo.js";
+import apiRoute from "./backend/src/routes/api/index.js";
+// import wakeUpDyno from "./backend//route/wakeUpDyno.js";
+
+dotenv.config();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = process.env.PORT || 80;
 
@@ -16,20 +20,24 @@ const port = process.env.PORT || 80;
 const app = express();
 
 app.use(cors());
-app.use("/api", apiRoute);
+app.use("/", apiRoute);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+mongo.connect();
 
+// const server = app.listen(process.env.PORT || 80, function(){
+//   console.log('on'+server.address().port);
+// });
+
+// server.applyMiddleware({ app });
 
 
 const httpServer = http.createServer(app);
 
-
-mongo.connect();
 
 httpServer.listen(port, () => {
   console.log(`🚀 Server Ready at ${port}! 🚀`);
